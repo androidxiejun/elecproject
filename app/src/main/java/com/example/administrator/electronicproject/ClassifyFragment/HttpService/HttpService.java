431fe16647196e41317eb6eb7cc11905f88a43e7
Administrator <1019163135@qq.com>
2016-09-06 10:21:46 +0800

    2016-09-06

---
+++ app/src/main/java/com/example/administrator/electronicproject/ClassifyFragment/HttpService/HttpService.java
@@ -0,0 +1,8 @@
+package com.example.administrator.electronicproject.ClassifyFragment.HttpService;
+
+/**
+ * Created by Administrator on 2016/9/6.
+ */
+public interface HttpService {
+
+}
---
+++ app/src/main/java/com/example/administrator/electronicproject/ClassifyFragment/HttpUtils/HttpUtils.java
@@ -0,0 +1,7 @@
+package com.example.administrator.electronicproject.ClassifyFragment.HttpUtils;
+
+/**
+ * Created by Administrator on 2016/9/6.
+ */
+public class HttpUtils {
+}
